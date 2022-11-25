a0243c5547a7d2fdb2b637a498eaa1e3a07b8d99
justanotherinternetguy <hendryxu1@gmail.com>
2022-11-25 08:24:46 -0500

    e

--- python/main.py
+++ python/main.py
@@ -14,6 +14,9 @@ class Seed():
         
     def euclideanDistance(self, x1, y1, x2, y2):
         return math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
+    
+    def manhattanDistance(self, x1, y1, x2, y2):
+        return abs(x2-x1) + abs(y2-y1)
 
     def setColor(self):
         self.r = random.randint(1, 255)
@@ -31,8 +34,11 @@ class Seed():
         return (self.x, self.y)
 
     def drawCenter(self):
-        self.draw.rectangle([(self.x-5, self.y-5), (self.x+5, self.y+5)], fill=(255, 255, 255))
+        self.draw.rectangle([(self.x-2, self.y-2), (self.x+2, self.y+2)], fill=(255, 255, 255))
 
+
+def compare_min(a, b, s):
+    if type(a) == None or a == None: return 1
 
 def generate_euclid(w, h, num_seeds):
     img = Image.new("RGB", (w, h))
@@ -51,24 +57,74 @@ def generate_euclid(w, h, num_seeds):
         seed1.setColor()
         seed1.setLocation(w, h)
         # draw.rectangle([(seed1.getLocation()[0]-50, seed1.getLocation()[1]-50), (seed1.getLocation()[0]+50, seed1.getLocation()[1]+50)], fill=(seed1.getColor()[0], seed1.getColor()[1], seed1.getColor()[2]))
-        seed1.drawCenter()
         seeds.append(seed1)
 
-    for seed in seeds:
-        for i in range(img_x):
-            for j in range(img_y):
-                # euclid distance pixel to every single seed
-                # record the min seed
-                # assign pixel to color of min seed
+    tmp = []
+    for i in range(img_x):
+        for j in range(img_y):
+            distances = []
+            nearest_seed = 9999999999999999999999999999
+            for s in range(num_seeds):
                 coords = (i, j)
-                seed_coords = seed.getLocation()
+                seed_coords = seeds[s].getLocation()
 
-                dist = seed.euclideanDistance(coords[0], coords[1], seed_coords[0], seed_coords[1])
-                print(dist)
+                dist = seeds[s].euclideanDistance(coords[0], coords[1], seed_coords[0], seed_coords[1])
+                distances.append(dist)
+                nearest = distances.index(min(distances))
+                putpixel(coords, seeds[nearest].getColor())
+                seeds[nearest].drawCenter()
+                # tmp.append(nearest)
 
-    img.save("test.png", "PNG")
+                
+
+
+    img.save("euclid.png", "PNG")
 
 
 
+def generate_manhattan(w, h, num_seeds):
+    img = Image.new("RGB", (w, h))
+    draw = ImageDraw.Draw(img)
+    putpixel = img.putpixel
+    img_x, img_y = img.size
 
-generate_euclid(500, 500, 1)
+    # seeds = [[], []]
+    # generate seeds
+    # draw.rectangle([(startx, starty), (endx, endy)], fill=(r, g, b))
+
+    seeds = []
+
+    for i in range(num_seeds):
+        seed1 = Seed(0, 0, 0, 0, 0, draw, putpixel)
+        seed1.setColor()
+        seed1.setLocation(w, h)
+        # draw.rectangle([(seed1.getLocation()[0]-50, seed1.getLocation()[1]-50), (seed1.getLocation()[0]+50, seed1.getLocation()[1]+50)], fill=(seed1.getColor()[0], seed1.getColor()[1], seed1.getColor()[2]))
+        seeds.append(seed1)
+
+    tmp = []
+    for i in range(img_x):
+        for j in range(img_y):
+            distances = []
+            nearest_seed = 9999999999999999999999999999
+            for s in range(num_seeds):
+                coords = (i, j)
+                seed_coords = seeds[s].getLocation()
+
+                dist = seeds[s].manhattanDistance(coords[0], coords[1], seed_coords[0], seed_coords[1])
+                distances.append(dist)
+                nearest = distances.index(min(distances))
+                putpixel(coords, seeds[nearest].getColor())
+                seeds[nearest].drawCenter()
+                # tmp.append(nearest)
+
+                
+
+
+    img.save("manhattan.png", "PNG")
+
+
+a = 500
+b = 500
+z = 4
+generate_manhattan(a, b, z)
+generate_euclid(a, b, z)
